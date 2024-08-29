ba8289f1e6fdfa35369ac6d8c6c22e82827ba0da
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-08-29 10:46:49 +0530

    2024.08.29

---
+++ 2024.08.26/1. Person/TestPerson.java
@@ -0,0 +1,11 @@
+public class TestPerson{
+	public static void main(String[] args){
+		
+		Student S1 = new Student("Dinusha Kasun", "Kandy","Computer Science", 2024, 13212);
+		System.out.println(S1);
+		
+		Staff Sf1 = new Staff("Upul", "Colombo", "abc school", 5400.3);
+		System.out.println(Sf1);
+		
+	}
+}
